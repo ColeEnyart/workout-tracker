eb2117d36f59859935100cb794a7d6331ce99d12
Cole Enyart <letchro@gmail.com>
2021-11-12 22:48:53 -0600

    Updated Workout model, removed exercise model

--- models/Workout.js
+++ models/Workout.js
@@ -2,17 +2,46 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
+const ExerciseSchema = new Schema({
     day: {
-        type: Date
+        type: Date,
+        default: Date.now
     },
-
-    exercises: {
-        type: Schema.Types.ObjectId,
-        ref: "Exercise"
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            duration: Number,
+            weight: {
+                type: Number,
+                default: 0
+            },
+            reps: {
+                type: Number,
+                default: 0
+            },
+            sets: {
+                type: Number,
+                default: 0
+            },
+            distance: {
+                type: Number,
+                default: 0
+            }
+        }
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0,
     }
 })
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
-module.exports = Workout;
+module.exports = Exercise;
