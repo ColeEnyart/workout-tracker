22d6398885df45e17432aa24e758c2300ec397e5
Cole Enyart <letchro@gmail.com>
2021-11-13 01:22:49 -0600

    Added api.js route

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const routes = require("./controllers");
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,8 +15,18 @@ app.use(express.static("public"));
 const opts = {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false};
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", opts);
 
-app.use(routes);
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
+
+// db.Workout.find({}).then(function (res) {
+//     console.log("Checking if db is populated");
+//     if (res.length === 0) {
+//         console.log("DB is empty");
+//         require("./seeders/seed.js");
+//     }
+// });
